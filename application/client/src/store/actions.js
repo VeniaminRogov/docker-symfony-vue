f6427d343b9aa982c287346572cfe5e48d2642f5
veniamin <rogov.veniamin@gmail.com>
2023-03-31 16:43:31 +0700

    add search by first letter

--- application/client/src/store/actions.js
+++ application/client/src/store/actions.js
@@ -5,3 +5,15 @@ export function searchMeals({ commit }, keyword) {
     commit("setSearchedMeals", data.meals);
   });
 }
+
+export function searchMealsByLetter({ commit }, letter) {
+  axiosClient.get(`search.php?f=${letter}`).then(({ data }) => {
+    commit("setMealsByLetter", data.meals);
+  });
+}
+
+export function searchMealsByIngredients({ commit }, ing) {
+  axiosClient.get(`list.php?c=list${ing}`).then(({ data }) => {
+    commit("setMealsByIngredient", data.meals);
+  });
+}
